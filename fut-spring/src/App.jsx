0b1b5b8a9d2c3269c31e61b139f5cc3aefdd09ce
Guga2111 <luisgosampaio@gmail.com>
2025-04-23 11:03:58 -0300

    feat: avatar padding config

--- fut-spring/src/App.jsx
+++ fut-spring/src/App.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect,useState} from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import axios from "axios";
 import './App.css'
 import PeladaGrid from './components/component/PeladaGrid';
@@ -22,12 +23,23 @@ function App() {
 
   return (
     <>
-      <div>
+    <div>
+      <div className='m-10'>
+      <Avatar className="absolute top-4 right-4 ...">
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+      </div>
+      <div className='pelada-grid'>
         <PeladaGrid peladas={peladas}></PeladaGrid>
       </div>
+      <div className='footer'>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      </div>
+    </div>
     </>
   )
 }

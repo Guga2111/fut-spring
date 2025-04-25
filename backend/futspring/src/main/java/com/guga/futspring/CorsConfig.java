448cbc262330580743629a96d51300e9ea77c837
Guga2111 <luisgosampaio@gmail.com>
2025-04-25 13:47:19 -0300

    feat: permit headers to visualize the JWT token in the frontend application

--- backend/futspring/src/main/java/com/guga/futspring/CorsConfig.java
+++ backend/futspring/src/main/java/com/guga/futspring/CorsConfig.java
@@ -6,17 +6,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer{
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*") //react url
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas as rotas
+                .allowedOrigins("http://localhost:5173") // Substitua pelo domínio do front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("Authorization", "Content-Type") // Cabeçalhos permitidos
+                .exposedHeaders("Authorization") // Exponha o cabeçalho Authorization
+                .allowCredentials(true); // Permite envio de cookies, se necessário
     }
 }

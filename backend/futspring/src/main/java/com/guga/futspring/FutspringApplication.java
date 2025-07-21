f00897aeb690d7cdd3bd5dd6029ef0cd67a48288
Guga2111 <luisgosampaio@gmail.com>
2025-07-21 13:53:22 -0300

    feat: CommandLineRunner bean for auto creating a "pelada" for optimization

--- backend/futspring/src/main/java/com/guga/futspring/FutspringApplication.java
+++ backend/futspring/src/main/java/com/guga/futspring/FutspringApplication.java
@@ -1,8 +1,9 @@
 package com.guga.futspring;
 
+import com.guga.futspring.entity.Daily;
+import com.guga.futspring.entity.Pelada;
 import com.guga.futspring.entity.User;
-import com.guga.futspring.service.UserService;
-import com.guga.futspring.service.UserServiceImpl;
+import com.guga.futspring.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @SpringBootApplication
 @EnableScheduling
@@ -26,7 +32,7 @@ public class FutspringApplication {
 	}
 
 	@Bean
-	public CommandLineRunner createInitialUsers(UserService userService) {
+	public CommandLineRunner createInitialUsers(UserServiceImpl userService, PeladaServiceImpl peladaService, DailyServiceImpl dailyService) {
 		return args -> {
 			System.out.println("----------------------------------------------");
 
@@ -50,6 +56,36 @@ public class FutspringApplication {
 			System.out.println("Total de usuários no banco de dados: " + userService.getUsers().size());
 			userService.getUsers().forEach(u -> System.out.println("ID: " + u.getId() + ", Username: " + u.getUsername() + ", Email: " + u.getEmail()));
 			System.out.println("----------------------------------------------");
+
+			if(peladaService.getPeladas().isEmpty() && dailyService.getDailys().isEmpty()) {
+				Pelada pelada = new Pelada();
+
+				pelada.setAddress("Abdias de Carvalho");
+				pelada.setName("Jogasse Onde?");
+				pelada.setReference("Ilha do retiro");
+				pelada.setPlayers(userService.getUsers());
+
+				pelada.setDuration(2);
+				pelada.setAutoCreateDailyEnabled(true);
+
+				LocalTime currentTime = LocalTime.now();
+				LocalTime peladaTime = currentTime.minusHours(1);
+				pelada.setTimeOfDay(peladaTime);
+
+				LocalDate today = LocalDate.now();
+				DayOfWeek currentDay = today.getDayOfWeek();
+				DayOfWeek peladaDay = currentDay.plus(1);
+				pelada.setDayOfWeek(peladaDay);
+
+				peladaService.savePelada(pelada, null);
+				System.out.println("Pelada criada com sucesso!");
+			} else {
+				System.out.println("O db ja tem uma pelada. Pulando a criação de pelada.");
+			}
+
+			peladaService.getPeladas().forEach(u -> System.out.println("ID: " + u.getId() + ", Username: " + u.getName() + ", Email: " + u.getAddress()));
+			System.out.println("----------------------------------------------");
+
 		};
 	}
 }

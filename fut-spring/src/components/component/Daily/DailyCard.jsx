9449e9c240b0efdec099c40aa4f45c964c83c8e1
Guga2111 <luisgosampaio@gmail.com>
2025-07-30 17:15:18 -0300

    feat: new handleClick for disconfirm presence in the daily

--- fut-spring/src/components/component/Daily/DailyCard.jsx
+++ fut-spring/src/components/component/Daily/DailyCard.jsx
@@ -47,6 +47,35 @@ export default function DailyCard({ pelada, onDailySelect, user }) {
     }
   };
 
+  const handleDisconfirmPresence = async () => {
+    if (!dailyScheduled || !user || !user.id) {
+      console.warn(
+        "Não foi possível confirmar presença: daily agendada ou ID do usuário não disponível."
+      );
+      return;
+    }
+
+    try {
+      const dailyId = dailyScheduled.id;
+      const userId = user.id;
+
+      const url = `http://localhost:8080/daily/${dailyId}/disconfirm-presence/${userId}`;
+
+      const response = await axios.put(url, {});
+
+      if (response.status === 200) {
+        toast.success(
+          `Presence of "${user.username}" disconfirmed successfully!`
+        );
+      } else {
+        console.error("Erro na resposta da API:", response.data);
+      }
+    } catch (error) {
+      console.error("Erro ao confirmar presença:", error);
+      toast.error("Failed to confirm your presence. Please try again.");
+    }
+  };
+
   const handleClick = () => {
     if (dailyScheduled) {
       onDailySelect(dailyScheduled);
@@ -133,7 +162,10 @@ export default function DailyCard({ pelada, onDailySelect, user }) {
           >
             CONFIRM
           </Button>
-          <Button className="px-6 font-semibold bg-black text-white hover:!bg-red-900 hover:!border-white">
+          <Button
+            className="px-6 font-semibold bg-black text-white hover:!bg-red-900 hover:!border-white"
+            onClick={handleDisconfirmPresence}
+          >
             DISCONFIRM
           </Button>
         </CardFooter>

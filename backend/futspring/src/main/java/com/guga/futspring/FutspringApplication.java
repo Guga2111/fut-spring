be343ded169873e098143ba771e19d3f8c1d1332
luisgustavo <luisgosampaio@gmail.com>
2025-08-13 20:27:36 -0300

    fix: for production stuff, commenting the 19 users creation, only used on the development testing

--- backend/futspring/src/main/java/com/guga/futspring/FutspringApplication.java
+++ backend/futspring/src/main/java/com/guga/futspring/FutspringApplication.java
@@ -34,29 +34,29 @@ public class FutspringApplication {
 	@Bean
 	public CommandLineRunner createInitialUsers(UserServiceImpl userService, PeladaServiceImpl peladaService, DailyServiceImpl dailyService) {
 		return args -> {
-			System.out.println("----------------------------------------------");
-
-			if (userService.getUsers().isEmpty()) {
-				for (int i = 1; i <= 19; i++) {
-					User user = new User();
-					user.setEmail("user" + i + "@example.com");
-					user.setUsername("User" + i);
-					user.setPassword("123");
-					user.setStars(5 - (i % 5));
-					user.setPosition("LW");
-
-
-					userService.saveUser(user);
-					System.out.println("Usuário criado: " + user.getUsername());
-				}
-				System.out.println("19 usuários iniciais foram criados com sucesso!");
-			} else {
-				System.out.println("O banco de dados já contém usuários. Pulando a criação de usuários iniciais.");
-			}
-
-			System.out.println("Total de usuários no banco de dados: " + userService.getUsers().size());
-			userService.getUsers().forEach(u -> System.out.println("ID: " + u.getId() + ", Username: " + u.getUsername() + ", Email: " + u.getEmail()));
-			System.out.println("----------------------------------------------");
+//			System.out.println("----------------------------------------------");
+//
+//			if (userService.getUsers().isEmpty()) {
+//				for (int i = 1; i <= 19; i++) {
+//					User user = new User();
+//					user.setEmail("user" + i + "@example.com");
+//					user.setUsername("User" + i);
+//					user.setPassword("123");
+//					user.setStars(5 - (i % 5));
+//					user.setPosition("LW");
+//
+//
+//					userService.saveUser(user);
+//					System.out.println("Usuário criado: " + user.getUsername());
+//				}
+//				System.out.println("19 usuários iniciais foram criados com sucesso!");
+//			} else {
+//				System.out.println("O banco de dados já contém usuários. Pulando a criação de usuários iniciais.");
+//			}
+//
+//			System.out.println("Total de usuários no banco de dados: " + userService.getUsers().size());
+//			userService.getUsers().forEach(u -> System.out.println("ID: " + u.getId() + ", Username: " + u.getUsername() + ", Email: " + u.getEmail()));
+//			System.out.println("----------------------------------------------");
 
 			/*if(peladaService.getPeladas().isEmpty() && dailyService.getDailys().isEmpty()) {
 				Pelada pelada = new Pelada();

4ac347ccbff32b94b4b0724271fd33ef50d52a6e
luisgustavo <luisgosampaio@gmail.com>
2025-08-20 14:36:36 -0300

    fix: language fix

--- frontend/fut-spring/src/components/component/ProfileNotLogged.jsx
+++ frontend/fut-spring/src/components/component/ProfileNotLogged.jsx
@@ -1,22 +1,27 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Link } from 'react-router-dom';
 
 export default function ProfileNotLogged() {
   return (
-    <div className="absolute top-4 right-4 bg-white">
+    <div className="absolute top-4 right-4 bg-gray-50">
       <div className="flex flex-row-reverse items-center gap-x-3 cursor-pointer">
         <Avatar className="w-8 h-8">
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>NF</AvatarFallback>
         </Avatar>
         <div className="text-xs leading-tight">
-          <a href="/login" className="text-green-600 hover:!underline">
+        <Link to="/register?tab=login">
+        <a className="text-green-600 hover:!underline">
             Login
           </a>{" "}
+      </Link>
           or
           <br />
-          <a href="/register" className="ml-5 text-green-600 hover:!underline">
+          <Link to={"/register?tab=register"}>
+          <a className="ml-5 text-green-600 hover:!underline">
             Register
           </a>
+          </Link>
         </div>
       </div>
     </div>
